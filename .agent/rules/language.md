26227f526ee1b66adaf74742e2bee7320e65d33d
M2mini <sjp0a.t@gmail.com>
2026-01-16 16:23:40 +0900

    first commit

---
+++ .agent/rules/language.md
@@ -0,0 +1,5 @@
+---
+trigger: always_on
+---
+
+日本語で表示して
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+// Small animation logic for cards if needed in the future
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    // We could add more dynamic effects here
+  });
+});
